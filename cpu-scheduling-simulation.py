311eab7d1324310889dbfbbbd31ba71777cdcb4f
Tenghoit Kouch <tenghoitkouch+github@gmail.com>
2026-02-17 13:07:57 -0500

    counter reordering

--- cpu-scheduling-simulation.py
+++ cpu-scheduling-simulation.py
@@ -2,6 +2,7 @@ import math
 from pathlib import Path
 from abc import ABC, abstractmethod
 import copy
+import logging
 
 class Job:
     def __init__(self, id, submitted_at, duration) -> None:
@@ -42,7 +43,7 @@ class PerformanceMetric:
 
 
     def __str__(self) -> str:
-        return f"Throughput: {self.throughput} | Turnaround: {self.turnaround} | Context Switches: {self.context_switches}"
+        return f"{self.turnaround}, {self.context_switches}"
 
 
 
@@ -56,21 +57,38 @@ class Scheduler(ABC):
         self.total_jobs: int = len(self.unfinished_jobs)
         self.time = 0
         self.total_context_switches: int = 0
+        self.added_jobs: bool = False
 
 
     def get_ready_jobs(self) -> None:
+        """
+        Add ready jobs to queue base on current time
+        """
         ready = [job for job in self.unfinished_jobs if job.submitted_at <= self.time]
+        if ready: self.added_jobs = True
         self.unfinished_jobs = [j for j in self.unfinished_jobs if j.submitted_at > self.time]
         self.queue.extend(ready)
 
 
     def work(self) -> None:
+        """
+        Work on current job and manages the finishing process
+        """
         if self.current_job is not None:
             self.current_job.work()
+
+        if self.current_job is not None and self.current_job.is_finished():
+            self.current_job.finish(self.time)
+            # print(f"Finished at {self.time}: {self.current_job}")
+            self.finished_jobs.append(self.current_job)
+            self.current_job = None
 
 
     @abstractmethod
     def context_switch(self) -> None:
+        """
+        Each scheduler will implement a different context_switch
+        """
         pass
 
 
@@ -81,58 +99,50 @@ class Scheduler(ABC):
 
     
     def run(self) -> PerformanceMetric:
+        """
+        Template Pattern shared across all schedulers
+        """
         while len(self.finished_jobs) != self.total_jobs:
-            # print(f"finished: {len(self.finished_jobs)}")
             self.get_ready_jobs()
             self.work()
             self.context_switch()
             self.time += 1
+            self.added_jobs = False
 
         return self.get_performance()
 
 
 
 class FCFS(Scheduler):
+    """
+    First-come First-served
+    """
     def __init__(self, jobs: list[Job]) -> None:
         super().__init__(jobs)
 
 
     def context_switch(self) -> None:
-
-        if self.current_job is not None and self.current_job.is_finished():
-            self.current_job.finish(self.time)
-            print(f"Finished at {self.time}: {self.current_job}")
-            self.finished_jobs.append(self.current_job)
-            self.current_job = None
+        if not self.queue: return
 
         if self.current_job is None:
-            if len(self.queue) == 0: return
-
-            self.current_job = self.queue.pop(0)
+            self.current_job = self.queue.pop(0) # get next job
             self.total_context_switches += 1
 
 
 
-
 class SJF(Scheduler):
+    """
+    Shortest Job First
+    """
     def __init__(self, jobs: list[Job]) -> None:
         super().__init__(jobs)
 
 
     def context_switch(self) -> None:
-        
-        
-        if self.current_job is not None and self.current_job.is_finished():
-            self.current_job.finish(self.time)
-            print(f"Finished at {self.time}: {self.current_job}")
-            self.finished_jobs.append(self.current_job)
-            self.current_job = None
+        if not self.queue: return
 
-        if self.current_job is None:
-            if len(self.queue) == 0: return
-
-
-            shortest_job = min(self.queue, key=lambda job: job.duration)
+        if self.current_job is None:      
+            shortest_job = min(self.queue, key=lambda job: job.duration) # get shortest job in queue
             self.queue.remove(shortest_job)
             self.current_job = shortest_job
             self.total_context_switches += 1
@@ -140,56 +150,55 @@ class SJF(Scheduler):
 
 
 class SRTN(Scheduler):
+    """
+    Shortest Runtime Next
+    """
     def __init__(self, jobs: list[Job]) -> None:
         super().__init__(jobs)
 
 
     def context_switch(self) -> None:
-        
-        
-        if self.current_job is not None and self.current_job.is_finished():
-            self.current_job.finish(self.time)
-            print(f"Finished at {self.time}: {self.current_job}")
-            self.finished_jobs.append(self.current_job)
-            self.current_job = None
-
-        if len(self.queue) == 0: return
+        if not self.queue: return
 
         shortest_runtime_job = min(self.queue, key=lambda job: job.currentDuration)
 
-        if self.current_job is not None and (self.current_job.currentDuration <= shortest_runtime_job.currentDuration):
-            return
-        
-        self.queue.remove(shortest_runtime_job)
-        if self.current_job is not None: self.queue.append(self.current_job)
-        self.current_job = shortest_runtime_job
-        self.total_context_switches += 1
+        if self.current_job is None:
+            self.queue.remove(shortest_runtime_job)
+            self.current_job = shortest_runtime_job
+            self.total_context_switches += 1
 
+        elif shortest_runtime_job.currentDuration < self.current_job.currentDuration:
+            self.queue.remove(shortest_runtime_job)
+            self.queue.append(self.current_job) # incomplete job gets added back to queue
+            self.current_job = shortest_runtime_job
+            self.total_context_switches += 1
+        
+        elif shortest_runtime_job.currentDuration >= self.current_job.currentDuration and self.added_jobs:
+            self.total_context_switches += 1
 
 
 
 class RR(Scheduler):
+    """
+    Round Robin
+    """
     def __init__(self, jobs: list[Job]) -> None:
         super().__init__(jobs)
 
 
     def context_switch(self) -> None:
-
-        if self.current_job is not None and self.current_job.is_finished():
-            self.current_job.finish(self.time)
-            print(f"Finished at {self.time}: {self.current_job}")
-            self.finished_jobs.append(self.current_job)
-            self.current_job = None
-
-        if len(self.queue) == 0: return
-
-        if self.current_job is not None: self.queue.append(self.current_job)
-        self.current_job = self.queue.pop(0)
         self.total_context_switches += 1
+        if not self.queue: return
+
+        if self.current_job is not None: self.queue.append(self.current_job) # incomplete job gets added back to queue
+        self.current_job = self.queue.pop(0)
 
 
 
 class Simulation:
+    """
+    Responsible for running experiments
+    """
     def __init__(self, data_path):
         self.jobs = self.read_data(data_path)
 
@@ -213,27 +222,20 @@ class Simulation:
 
 
     def run(self):
-        jobs_copy = copy.deepcopy(self.jobs)
-        # manager = FCFS(jobs_copy)
-        # manager = SJF(jobs_copy)
-        # manager = SRTN(jobs_copy)
-        manager = RR(jobs_copy)
-
-        result = manager.run()
-
-        print(result)
+        schedulers = [FCFS, SJF, SRTN, RR]
+        print(f"algorithm, turnaround, context_switches")
+        for scheduler in schedulers:
+            jobs_copy = copy.deepcopy(self.jobs)
+            manager = scheduler(jobs_copy)
+            result = manager.run()
+            print(f"{scheduler.__name__}, {result}")
 
 
 
 def main():
     data_path = Path("data.txt")
-
     world: Simulation = Simulation(data_path=data_path)
-
     world.run()
-
-
-
 
 
 if __name__ == "__main__":
